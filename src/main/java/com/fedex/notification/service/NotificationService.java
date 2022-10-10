668b3cdd32391a95f8576c13bcfc4e5e0cd66c6e
sarojgharat <saroj.gharat@gmail.com>
2022-10-10 09:03:59 +0200

    Update NotificationService.java

--- src/main/java/com/fedex/notification/service/NotificationService.java
+++ src/main/java/com/fedex/notification/service/NotificationService.java
@@ -14,10 +14,13 @@ public class NotificationService {
 	@Autowired
 	UserPreferenceGateway userPreferenceGateway;
 
-	public String  sendWhtsAppNotification(long trackingId) {
-		UserProfile userProfile = userPreferenceGateway.getUserProfile(trackingId);
+	public String  sendWhtsAppNotification(String userId) {
+		UserProfile userProfile = userPreferenceGateway.getUserProfile(userId);
 		String whtsAppNo = String.valueOf(userProfile.getCountry_code()) + String.valueOf(userProfile.getPhone_no());
+		long trackingId = (long) Math.floor(Math.random() * 9_000_000_000L) + 1_000_000_000L;
 		return whtsAppNotificationGateway.sendNotification(whtsAppNo, userProfile.getName(),userProfile.getPreferred_lang(),trackingId);
 	}
+	
+	
 
 }
