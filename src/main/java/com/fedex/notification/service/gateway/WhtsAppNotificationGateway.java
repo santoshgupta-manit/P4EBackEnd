d0d367bbc6af65c61344b1348b4fc74e187ebe76
sarojgharat <saroj.gharat@gmail.com>
2022-10-10 09:05:06 +0200

    Update WhtsAppNotificationGateway.java

--- src/main/java/com/fedex/notification/service/gateway/WhtsAppNotificationGateway.java
+++ src/main/java/com/fedex/notification/service/gateway/WhtsAppNotificationGateway.java
@@ -9,27 +9,45 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
-public class WhtsAppNotificationGateway
-{
+public class WhtsAppNotificationGateway {
 	@Autowired
 	private RestTemplate restTemplate;
-	
+
 	@Value("${notification.service.url}")
 	private String notification_api_url;
-	
+
 	@SuppressWarnings("unlikely-arg-type")
-	public String sendNotification(String whtsAppNo, String custName, String lang, long trackingId  )
-	{
-		//To be implemented
-		String url=notification_api_url+"?whtsappno="+whtsAppNo+"&name="+custName+"&trackingId="+trackingId+"&lang="+lang;
-		System.out.println("url :"+url);
+	public String sendNotification(String whtsAppNo, String custName, String lang, long trackingId) {
+
+		/*
+		TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+
+		SSLContext sslContext;
+		try {
+			sslContext = org.apache.http.ssl.SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy)
+					.build();
+			SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
+			CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).build();
+
+			HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+			requestFactory.setHttpClient(httpClient);
+			restTemplate.setRequestFactory(requestFactory);
+		} catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+
+		}
+		*/
+		// To be implemented
+		String url = notification_api_url + "?whtsappno=" + whtsAppNo + "&name=" + custName + "&trackingId="
+				+ trackingId + "&lang=" + lang;
+		System.out.println("url :" + url);
 		RequestEntity<Void> request = RequestEntity.get(url).accept(MediaType.APPLICATION_JSON).build();
 		ResponseEntity<String> response = restTemplate.exchange(request, String.class);
-		if(response.getStatusCode().is2xxSuccessful())
-		{
+		if (response.getStatusCode().is2xxSuccessful()) {
 			return "Message sent successfully.";
 		}
 		return "Could not sent message.";
-		
+
 	}
 }
